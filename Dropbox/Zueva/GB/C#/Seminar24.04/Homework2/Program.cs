694541d7676d7caf36120b56831548f53caf1c42
Daria <dashazoueva@gmail.com>
2022-04-26 18:02:24 +0300

    Homework2

---
+++ Dropbox/Zueva/GB/C#/Seminar24.04/Homework2/Program.cs
@@ -0,0 +1,22 @@
+﻿string s_i = Console.ReadLine();
+double i = double.Parse(s_i);
+double b;
+b = 1/i;
+
+if(i>0)
+
+{
+    Console.Write(b);
+}
+
+if(i<0)
+
+{
+    Console.Write(b);
+}
+
+if(i==0)
+
+{
+    Console.Write(i);
+}
