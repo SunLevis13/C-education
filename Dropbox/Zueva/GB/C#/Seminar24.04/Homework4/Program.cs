ab52210d7d4d6739b1023d3afc49def8a014752a
Daria <dashazoueva@gmail.com>
2022-04-26 18:38:50 +0300

    Homework4

---
+++ Dropbox/Zueva/GB/C#/Seminar24.04/Homework4/Program.cs
@@ -0,0 +1,14 @@
+﻿string s_a = Console.ReadLine();
+int a = int.Parse(s_a);
+string s_b = Console.ReadLine();
+int b = int.Parse(s_b);
+string s_c = Console.ReadLine();
+int c = int.Parse(s_c);
+
+int max = a;
+
+if(b > max) max = b;
+if(c > max) max = c;
+
+Console.Write("max = ");
+Console.Write(max);
