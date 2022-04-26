d11a7bfc63482d0e638cca5fe2a3b1d2306ad63a
Daria <dashazoueva@gmail.com>
2022-04-26 17:44:15 +0300

    homework1

---
+++ Dropbox/Zueva/GB/C#/Seminar24.04/Homework1/Program.cs
@@ -0,0 +1,15 @@
+﻿string s_a = Console.ReadLine();
+int a = int.Parse(s_a);
+if(a < 0)
+
+{
+    Console.Write($"{-a} ");
+    a += 1;
+}
+else
+if(a>=0)
+
+{
+    Console.Write($"{a} ");
+    a += 1;
+}
