ee139de157622c1821ae84a8ff7ee090afe10d8d
Daria <dashazoueva@gmail.com>
2022-04-26 17:46:04 +0300

    Programs

---
+++ Dropbox/Zueva/GB/C#/Seminar24.04/Program3/Program.cs
@@ -0,0 +1,31 @@
+﻿string s_a = Console.ReadLine();
+int a = int.Parse(s_a);
+
+if (a == 1)
+{
+    Console.WriteLine("Monday");
+}
+if (a == 2)
+{
+    Console.WriteLine("Tuesday");
+}
+if (a == 3)
+{
+    Console.WriteLine("Wednesday");
+}
+if (a == 4)
+{
+    Console.WriteLine("Thursday");
+}
+if (a == 5)
+{
+    Console.WriteLine("Friday");
+}
+if (a == 6)
+{
+    Console.WriteLine("Saturday");
+}
+if (a == 7)
+{
+    Console.WriteLine("Sunday");
+}
---
+++ Dropbox/Zueva/GB/C#/Seminar24.04/Program4/Program.cs
@@ -0,0 +1,6 @@
+﻿string s_x = Console.ReadLine();
+int x = int.Parse(s_x);
+int y;
+
+y = -2 * (x * x) + 4 * x + 2;
+Console.WriteLine(y);
