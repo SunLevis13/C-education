20abe5d556692b2571f55b2dbe3c169b472a5a33
Daria <dashazoueva@gmail.com>
2022-04-27 15:35:26 +0300

    tasks

---
+++ task3/Program.cs
@@ -0,0 +1,13 @@
+﻿int [] array = {1,12,31,4,15,16,17,18,31};
+int n = array.Length;
+int find = 31;
+int index = 0;
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break; //заканчивает цикл и показывает только первый элемент
+    }
+    index++; //аналогично index = index + 1
+}
